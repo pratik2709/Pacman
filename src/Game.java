ca98b2a84e6c4210fd978cb7c48500a79b6b877b
pratiksaxena <pratik2709@gmail.com>
2017-10-16 14:29:06 +0530

    Created screen size, thread and thread methods

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,24 @@ public class Game extends Canvas implements Runnable{
 
     private boolean isRunning = false;
 
+    public  static final int WIDTH = 640, HEIGHT = 480;
+
+    public static final String Title = "Pac-Man";
+
+    private Thread thread;
+
+    public Game(){
+
+    }
+
+    public synchronized void start(){
+
+    }
+
+    public synchronized void stop(){
+
+    }
+
     public void tick(){
 
     }
