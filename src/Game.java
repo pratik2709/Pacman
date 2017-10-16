70543e68f2a1199c1714e76f44d1123380aabcfb
pratiksaxena <pratik2709@gmail.com>
2017-10-16 23:08:55 +0530

    understood delta time and added code for that. Also started with the render method with buffers

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable{
 
@@ -12,7 +13,10 @@ public class Game extends Canvas implements Runnable{
     private Thread thread;
 
     public Game(){
-
+        Dimension dimension = new Dimension(Game.WIDTH, Game.HEIGHT);
+        setPreferredSize(dimension);
+        setMaximumSize(dimension);
+        setMinimumSize(dimension);
     }
 
     public synchronized void start(){
@@ -36,26 +40,67 @@ public class Game extends Canvas implements Runnable{
     }
 
     public void tick(){
-
     }
 
     public void render(){
+        //https://stackoverflow.com/questions/13590002/understand-bufferstrategy
+        BufferStrategy bs = getBufferStrategy();
+        if(bs == null){
+            createBufferStrategy(3);
+            return;
+        }
 
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
+
+        g.dispose();
+        bs.show();
     }
 
     @Override
     public void run(){
+
+        // need to understand how timing works in games
+        // delta is time between 2 frames
+        //http://gameprogrammingpatterns.com/game-loop.html
+        int fps = 0;
+        double timer = System.currentTimeMillis();
+        long lastTime = System.nanoTime();
+        double targetTick = 60.0;
+        double delta = 0;
+        //number of nanoseconds per frame
+        double ns = 1000000000/targetTick;
+
+
         while(isRunning){
-            tick();
-            render();
+            long now = System.nanoTime();
+            delta += (now - lastTime)/ns;
+            lastTime = now;
+
+            // other than 1 the rest of the time the computer sleeps/skips
+            while (delta >= 1){
+                tick();
+                render();
+                fps++;
+                delta--;
+            }
+
+            if(System.currentTimeMillis() - timer >= 1000){
+                System.out.println(fps);
+                fps = 0;
+                timer += 1000;
+            }
 
         }
+        stop();
     }
 
     public static void main(String[] args){
         Game game = new Game();
         JFrame frame = new JFrame();
-        frame.setTitle(game.Title);
+        frame.setTitle(Game.Title);
         frame.add(game);
         frame.setResizable(false);
         frame.pack();
