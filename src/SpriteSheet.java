df4f7e75ceac0fd4113bfedc69c081ac6b4126c9
pratiksaxena <pratik2709@gmail.com>
2017-10-19 01:28:44 +0530

    intermediate for sprite sheet

---
+++ src/SpriteSheet.java
@@ -0,0 +1,22 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class SpriteSheet {
+
+    private BufferedImage sheet;
+
+    public SpriteSheet(String path){
+        try{
+            sheet = ImageIO.read(getClass().getResource(path));
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public BufferedImage getSprite(int xx, int yy){
+        //because each sub image is 16*16
+        return sheet.getSubimage(xx, yy, xx+16, yy+16);
+    }
+}
